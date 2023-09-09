f5c69e79ad1995981647a46aff5f4dd4b0cebfd0
Matt McKelvey <mmckelve45@gmail.com>
2023-09-09 12:16:54 -0700

    Finished quiz

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,14 +1,24 @@
 import React, { useEffect } from "react";
 
 export default function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
   useEffect(
     function () {
-      setInterval(() => {
+      const id = setInterval(() => {
         dispatch({ type: "tick" });
         // console.log('tick')
       }, 1000);
+      return () => clearInterval(id);
     },
     [dispatch]
   );
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
